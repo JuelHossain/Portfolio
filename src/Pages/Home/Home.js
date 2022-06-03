075a2d18036f8d52c715493c3b439082187f3725
juelhossain <juelhossainnn@gmail.com>
2022-06-03 21:59:31 +0600

    work done for today

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Services from '../Services/Services';
 import Banner from './Banner';
+import SkillsOverView from './SkillsOverView';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Services />
-            
+            <SkillsOverView/>
         </div>
     );
 };
--- src/Pages/Home/SkillsOverView.js
+++ src/Pages/Home/SkillsOverView.js
@@ -1,10 +1,60 @@
+import { Container,  Heading, SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
+
+import SkillsCard from './SkillsCard';
+import {DiReact} from 'react-icons/di'
 
 const SkillsOverView = () => {
     return (
-        <div>
-            
-        </div>
+      <Container maxW={"container.lg"}>
+        <Heading py={6} size={"md"} className="text-center">
+          What I Know ?
+        </Heading>
+        <hr className="mb-6 mx-60" />
+
+      <SimpleGrid columns={[2, 3, 4,5]} spacing="2">
+          <SkillsCard
+            photo={
+              "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"
+            }
+            title="React"
+            text={"The Most Popular Javascript Library"}
+            icon={<DiReact />}
+          />
+          <SkillsCard
+            photo={
+              "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"
+            }
+            title="Express Js"
+            text={"Most Popular Node js Framework"}
+            icon={<DiReact />}
+          />
+          <SkillsCard
+            photo={
+              "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"
+            }
+            title="Web Driver IO"
+            text={"Most Popular Javascript Automation Framework"}
+            icon={<DiReact />}
+          />
+          <SkillsCard
+            photo={
+              "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"
+            }
+            title="Illustrator"
+            text={"Industry Standard Illustration Tool."}
+            icon={<DiReact />}
+          />
+          <SkillsCard
+            photo={
+              "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"
+            }
+            title="Tailwind Css"
+            text={"The Most Popular Css Framework."}
+            icon={<DiReact />}
+          />
+            </SimpleGrid>
+      </Container>
     );
 };
 
